04335166439238bb12cf62fe356636cd91e96379
mliu-cs <39929451+mliu-cs@users.noreply.github.com>
2019-05-06 14:46:22 -0700

    first working pass, seems like all client methods are now proxied (#185)
    * first working pass, seems like all client methods are now proxied
    
    * added bypass proxy env variable

--- src/client/catalog.js
+++ src/client/catalog.js
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-const request = require('superagent');
+const  { request } = require('../commands/apiutils');
 const debug = require('debug')('cortex:cli');
 const _ = require('lodash');
 const chalk = require('chalk');
