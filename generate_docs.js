1fe575665305e936c81675cd6a78ddbc4961ec5e
Johan Gielstra <jgielstra@cognitivescale.com>
2020-08-03 10:30:39 -0500

    added header

--- generate_docs.js
+++ generate_docs.js
@@ -41,13 +41,47 @@ cortex ${cmd.name} ${cmd.usage}
 ${cmd.description}  
 `;
 
-const output = _.join(
+const header = `
+---
+title: "Cortex CLI"
+linkTitle: "Cortex CLI"
+description: >
+  Provides a list of all commands available in the Cortex Command Line Interface.
+---
+This reference guide provides a list of all commands available in the Cortex Command Line
+Interface (Cortex CLI). See [CLI](getting-started/use-cli.md) for
+help getting started using the CLI.
+## Notation
+The following table describes the common notation used in this reference guide.
+| Notation | Description  | Example                                       |
+| -------- | -----------  | -------                                       |
+| \`< >\`    | Required     | \`<value>\` denotes a required value.      |
+| \`[ ]\`    | Optional     | \`[options]\` denotes an optional value.        |
+## Syntax
+\`\`\`cli
+cortex [command] [options]
+\`\`\`
+## Global options
+| Option | Description |
+| --------- | --------- |
+| \`-v\`, \`--version\` | Outputs the version number. |
+| \`-h\`, \`--help\` | Outputs usage information. |
+| \`--profile [profile]\` | Run the CLI using the specified profile instead of the default config. |
+## General notes
+* The output results for most \`list\` commands display as a table. Add the
+\`--json\` option to output results as JSON. When \`--json\` is used, you can use the
+\`--query\` option to [filter the results](/getting-started/use-cli.md).
+* The output results for \`describe\` commands display in the format of the document
+it is describing. This can include YAML, JSON, or other formats.
+`
+
+const body = _.join(
     _.sortBy(rootJson, ["name"]).map(
         (cmd) =>`${cmdHeading(cmd)}\n`+
             `${subcmdTable(require(`${sourcedir}/cortex-${cmd.name}.json`))}`
         ),
 '');
-
+const output = header + body
 // if outfile write to file ...
 if (_.isEmpty(outfile)) {
     console.log(output)
