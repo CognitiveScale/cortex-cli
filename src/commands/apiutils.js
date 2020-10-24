71d47a9023876ae34f7468bb2c57b1d4f9f36515
Michael Criscolo <mcriscolo@cognitivescale.com>
2020-10-24 13:11:57 -0500

    FAB-285 handle auth error

--- src/commands/apiutils.js
+++ src/commands/apiutils.js
@@ -6,9 +6,15 @@ const debug = require('debug')('cortex:cli');
 const { printWarning } = require('./utils');
 const { getUserAgent } = require('../useragent');
 
+function handleAuthError(res, req) {
+    if (res.headers['x-auth-error']) {
+        res.statusCode = 403;
+        this.callback(new Error(`Auth Error: ${res.headers['x-auth-error']}`));
+    }
+}
 /**
  * makes a request via superagent. proxy methods will be utilized
- * if the user (i.e. person running cli commands) has set environment 
+ * if the user (i.e. person running cli commands) has set environment
  * variables for their proxy (this is assuming ofc they have a proxy). if
  * no env variables set, the request just keeps piping through.
  */
@@ -23,38 +29,54 @@ function requestWrapper() {
     const bypassProxy = process.env.bypass_proxy;
 
     this.get = function(endpoint) {
-        if (bypassProxy) return superagentRequest.get(endpoint);
+        if (bypassProxy)
+            return superagentRequest
+                .get(endpoint)
+                .on('redirect', handleAuthError);
 
         bothProxiesSet && printWarning(bothProxiesWarning);
         return superagentRequest
             .get(endpoint)
             .proxy(proxyEnv)
+            .on('redirect', handleAuthError)
             .set('User-Agent', getUserAgent());
     };
     this.delete = function(endpoint) {
-        if (bypassProxy) return superagentRequest.delete(endpoint);
+        if (bypassProxy)
+            return superagentRequest
+                .delete(endpoint)
+                .on('redirect', handleAuthError);
 
         bothProxiesSet && printWarning(bothProxiesWarning);
         return superagentRequest
             .delete(endpoint)
+            .on('redirect', handleAuthError)
             .proxy(proxyEnv)
             .set('User-Agent', getUserAgent());
     };
     this.post = function(endpoint) {
-        if (bypassProxy) return superagentRequest.post(endpoint);
+        if (bypassProxy)
+            return superagentRequest
+                .post(endpoint)
+                .on('redirect', handleAuthError);
 
         bothProxiesSet && printWarning(bothProxiesWarning);
         return superagentRequest
             .post(endpoint)
+            .on('redirect', handleAuthError)
             .proxy(proxyEnv)
             .set('User-Agent', getUserAgent());
     }
     this.put = function(endpoint) {
-        if (bypassProxy) return superagentRequest.put(endpoint);
+        if (bypassProxy)
+            return superagentRequest
+                .put(endpoint)
+                .on('redirect', handleAuthError);
 
         bothProxiesSet && printWarning(bothProxiesWarning);
         return superagentRequest
             .put(endpoint)
+            .on('redirect', handleAuthError)
             .proxy(proxyEnv)
             .set('User-Agent', getUserAgent());
     }
