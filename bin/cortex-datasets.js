840e8d86e4da424cd06a2dfaf2bd9e4721c07393
Johan Gielstra <jgielstra@cognitivescale.com>
2020-10-25 13:42:30 -0500

    Removed cortex datasets generate

--- bin/cortex-datasets.js
+++ bin/cortex-datasets.js
@@ -117,18 +117,4 @@ program
         }
     }));
 
-program
-    .command('generate')
-    .description('Generates the structure and top level build script for a dataset')
-    .option('--color [on/off]', 'Turn on/off colors for JSON output.', 'on')
-    .option('--profile [profile]', 'The profile to use', 'default')
-    .option('--project [project]', 'The project to use')
-    .action((options) => { // deliberately not using withCompatibilityCheck()
-        try {
-            new GenerateDatasetCommand(program).execute(options);
-        } catch (err) {
-            console.error(chalk.red(err.message));
-        }
-    });
-
 program.parse(process.argv);
