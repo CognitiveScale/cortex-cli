4b8bae294c9cd4bdc08c154f272f10fa45642dfb
ljha <ljha@cognitivescale.com>
2020-11-12 16:16:48 +0530

    formatting fix

--- src/commands/skills.js
+++ src/commands/skills.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 Cognitive Scale, Inc. All Rights Reserved.
+ * Copyright 2020 Cognitive Scale, Inc. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the “License”);
  * you may not use this file except in compliance with the License.
@@ -19,29 +19,27 @@ const yeoman = require('yeoman-environment');
 const debug = require('debug')('cortex:cli');
 const { loadProfile } = require('../config');
 const Catalog = require('../client/catalog');
-const { printSuccess, printError, filterObject, parseObject, printTable } = require('./utils');
+const {
+ printSuccess, printError, filterObject, parseObject, printTable,
+} = require('./utils');
 
-function _formatpath(p){
-    let cnt=0, res = '';
+function _formatpath(p) {
+    let cnt = 0; let
+res = '';
     const len = p.length;
-    p.forEach(s => {
+    p.forEach((s) => {
         if (_.isNumber(s)) {
-            res += `[${s}]`
-        }else
-            if ( cnt < len)
-                res += s;
-            else
-                res +=s;
-        if (cnt < len-1 && !_.isNumber(p[cnt+1]))
-            res += '.';
+            res += `[${s}]`;
+        } else
+            if (cnt < len) res += s;
+            else res += s;
+        if (cnt < len - 1 && !_.isNumber(p[cnt + 1])) res += '.';
         cnt += 1;
-
-    })
+    });
     return res;
 }
 
 module.exports.SaveSkillCommand = class SaveSkillCommand {
-
     constructor(program) {
         this.program = program;
     }
@@ -54,23 +52,20 @@ module.exports.SaveSkillCommand = class SaveSkillCommand {
         const skill = parseObject(skillDefStr, options);
 
         const catalog = new Catalog(profile.url);
-        catalog.saveSkill(profile.token, skill).then((response) => {
+        catalog.saveSkill(options.project || profile.project, profile.token, skill).then((response) => {
             if (response.success) {
-                printSuccess(`Skill saved`, options);
-            } else {
-                if (response.details) {
+                printSuccess('Skill saved', options);
+            } else if (response.details) {
                     console.log(`Failed to save skill: ${response.status} ${response.message}`);
                     console.log('The following issues were found:');
                     const tableSpec = [
-                        {column: 'Path', field: 'path', width: 50},
-                        {column: 'Message', field: 'message', width: 100},
+                        { column: 'Path', field: 'path', width: 50 },
+                        { column: 'Message', field: 'message', width: 100 },
                     ];
                     response.details.map(d => d.path = _formatpath(d.path));
-                    printTable(tableSpec,response.details);
+                    printTable(tableSpec, response.details);
                     printError(''); // Just exit
-
                 }
-            }
         })
         .catch((err) => {
             printError(`Failed to save skill: ${err.status} ${err.response.body.error}`, options);
@@ -79,7 +74,6 @@ module.exports.SaveSkillCommand = class SaveSkillCommand {
 };
 
 module.exports.ListSkillsCommand = class ListSkillsCommand {
-
     constructor(program) {
         this.program = program;
     }
@@ -89,26 +83,23 @@ module.exports.ListSkillsCommand = class ListSkillsCommand {
         debug('%s.executeListSkills()', profile.name);
 
         const catalog = new Catalog(profile.url);
-        catalog.listSkills(profile.token).then((response) => {
+        catalog.listSkills(options.project || profile.project, profile.token).then((response) => {
             if (response.success) {
                 let result = response.skills;
-                if (options.query)
-                    result = filterObject(result, options);
+                if (options.query) result = filterObject(result, options);
 
                 if (options.json) {
                     printSuccess(JSON.stringify(result, null, 2), options);
-                }
-                else {
+                } else {
                     const tableSpec = [
                         { column: 'Title', field: 'title', width: 50 },
                         { column: 'Name', field: 'name', width: 50 },
-                        { column: 'Version', field: '_version', width: 12 }
+                        { column: 'Version', field: '_version', width: 12 },
                     ];
 
                     printTable(tableSpec, result);
                 }
-            }
-            else {
+            } else {
                 printError(`Failed to list skills: ${response.status} ${response.message}`, options);
             }
         })
@@ -119,7 +110,6 @@ module.exports.ListSkillsCommand = class ListSkillsCommand {
 };
 
 module.exports.DescribeSkillCommand = class DescribeSkillCommand {
-
     constructor(program) {
         this.program = program;
     }
@@ -129,12 +119,11 @@ module.exports.DescribeSkillCommand = class DescribeSkillCommand {
         debug('%s.executeDescribeSkill(%s)', profile.name, skillName);
 
         const catalog = new Catalog(profile.url);
-        catalog.describeSkill(profile.token, skillName).then((response) => {
+        catalog.describeSkill(options.project || profile.project, profile.token, skillName).then((response) => {
             if (response.success) {
-                let result = filterObject(response.skill, options);
+                const result = filterObject(response.skill, options);
                 printSuccess(JSON.stringify(result, null, 2), options);
-            }
-            else {
+            } else {
                 printError(`Failed to describe skill ${skillName}: ${response.message}`, options);
             }
         })
