8f7ee2016d65e6118232be842e8fa3ccae4ea6cf
ljha <ljha@cognitivescale.com>
2020-11-17 20:30:24 +0530

    skill type template

--- src/commands/plopfile.js
+++ src/commands/plopfile.js
@@ -22,8 +22,8 @@ module.exports = plop => {
                 // Current directory for the new files
                 destination: path.join(path.resolve('.'), 'src/{{name}}/'),
                 // Handlebars template used to generate content of project files
-                base: path.join(path.dirname(require.resolve('@c12e/generator-cortex/generators/skill'))),
-                templateFiles: path.join(path.dirname(require.resolve('@c12e/generator-cortex/generators/skill')), '**/*.js.hbs'),
+                base: path.join(path.dirname(require.resolve('@c12e/generator-cortex/generators/skill/{{type}}'))),
+                templateFiles: path.join(path.dirname(require.resolve('@c12e/generator-cortex/generators/skill/{{type}}')), '**/*.js.hbs'),
                 abortOnFail: true,
                 // Must not overwrite files with scaffolding template if already exists
                 // force: true,
