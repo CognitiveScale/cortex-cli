af373aa7d5c42a994815515a815c39ea6019edf1
Johan Gielstra <jgielstra@cognitivescale.com>
2020-12-04 14:21:35 -0600

    Allow json file for action save

--- bin/cortex-actions.js
+++ bin/cortex-actions.js
@@ -136,33 +136,24 @@ program
 
 // Deploy Action
 program
-    .command('deploy <actionName>')
+    .command('deploy [actionDefinition]')
     .alias('save')
     .description('Deploy an action')
-    .option('--no-compat', 'Ignore API compatibility checks')
-    .option('--docker [image]', 'Docker image to use as the runner')
-    .option('--ttl [ttl]', 'Daemon time to live')
+    .option('--actionType [actionType]', 'Type of action')
+    .option('--cmd [cmd]', 'Command to be executed') // '["--daemon"]'
     .option('--color [on/off]', 'Turn on/off colors for JSON output.', 'on')
+    .option('--docker [image]', 'Docker image to use as the runner')
+    .option('--environmentVariables [environmentVariables]', 'Docker container environment variables, only used for daemon action types')
+    .option('--actionName [name]', 'Action name')
+    .option('--no-compat', 'Ignore API compatibility checks')
+    .option('--podspec [podspec]', 'A file containing either a JSON or YAML formatted pod spec to merge with the action definition, used for specifying resources (like memory, ephemeral storage, CPUs, and GPUs) and tolerations (like allowing pods to be scheduled on tainted nodes).')
+    .option('--port [port]', 'Docker port') // '9091'
     .option('--profile [profile]', 'The profile to use')
     .option('--project [project]', 'The project to use')
-    .option('--actionType [actionType]', 'Type of action')
-    .option('--port [port]', 'Docker port') // '9091'
-    .option('--cmd [cmd]', 'Command to be executed') // '["--daemon"]'
-    .option('--environmentVariables [environmentVariables]', 'Docker container environment variables, only used for daemon action types')
     .option('--push-docker', 'Push Docker image to the Cortex registry.')
-    .option('--scaleCount [count]', 'Scale count', 'Scale count, only used for daemon action types')
-    .option('--podspec [podspec]', 'A file containing either a JSON or YAML formatted pod spec to merge with the action definition, '
-        + 'used for specifying resources (like memory, ephemeral storage, CPUs, and GPUs) and tolerations (like allowing pods to be scheduled on tainted nodes).')
+    .option('--scaleCount [count]', 'Scale count, only used for daemon action types')
     .action(withCompatibilityCheck((actionName, options) => {
         try {
-            if (!options.docker) {
-                throw new Error('--docker [image] required');
-            }
-            // allow kind with docker for blackbox images..
-            // if (options.docker && options.kind) {
-            //     throw new Error('Use either --kind [kind] or --docker [image], but not both');
-            // }
-
             new DeployActionCommand(program).execute(actionName, options);
         } catch (err) {
             console.error(chalk.red(err.message));
