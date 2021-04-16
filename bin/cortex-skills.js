a952a85a8b7a0ef6334b83a1ba3be10105ed341d
Johan Gielstra <jgielstra@cognitivescale.com>
2021-04-16 09:26:49 -0500

    Add skills deploy/undeploy cli commands

--- bin/cortex-skills.js
+++ bin/cortex-skills.js
@@ -26,40 +26,22 @@ const {
     ListSkillsCommand,
     DescribeSkillCommand,
     InvokeSkillCommand,
+    DeploySkillCommand,
+    UndeploySkillCommand,
 } = require('../src/commands/skills');
 
 program.description('Work with Cortex Skills');
 
-// Save Skill
+// Deploy Skill
 program
-    .command('save <skillDefinition>')
-    .description('Save a skill definition')
+    .command('deploy <skillName>')
+    .description('Deploy the skill resource to the cluster')
     .option('--no-compat', 'Ignore API compatibility checks')
-    .option('--color [on/off]', 'Turn on/off colors for JSON output.', 'on')
     .option('--profile [profile]', 'The profile to use')
     .option('--project [project]', 'The project to use')
-    .option('-y, --yaml', 'Use YAML for skill definition format')
-    .action(withCompatibilityCheck((skillDefinition, options) => {
+    .action(withCompatibilityCheck((skillName, options) => {
         try {
-            new SaveSkillCommand(program).execute(skillDefinition, options);
-        } catch (err) {
-            console.error(chalk.red(err.message));
-        }
-    }));
-
-// List Skills
-program
-    .command('list')
-    .description('List skill definitions')
-    .option('--no-compat', 'Ignore API compatibility checks')
-    .option('--color [on/off]', 'Turn on/off colors for JSON output.', 'on')
-    .option('--profile [profile]', 'The profile to use')
-    .option('--project [project]', 'The project to use')
-    .option('--json', 'Output results using JSON')
-    .option('--query [query]', 'A JMESPath query to use in filtering the response data.')
-    .action(withCompatibilityCheck((options) => {
-        try {
-            new ListSkillsCommand(program).execute(options);
+            new DeploySkillCommand(program).execute(skillName, options);
         } catch (err) {
             console.error(chalk.red(err.message));
         }
@@ -97,6 +79,56 @@ program
     .action(withCompatibilityCheck((skillName, inputName, options) => {
         try {
             new InvokeSkillCommand(program).execute(skillName, inputName, options);
+        } catch (err) {
+            console.error(chalk.red(err.message));
+        }
+    }));
+
+// List Skills
+program
+    .command('list')
+    .description('List skill definitions')
+    .option('--no-compat', 'Ignore API compatibility checks')
+    .option('--color [on/off]', 'Turn on/off colors for JSON output.', 'on')
+    .option('--profile [profile]', 'The profile to use')
+    .option('--project [project]', 'The project to use')
+    .option('--json', 'Output results using JSON')
+    .option('--query [query]', 'A JMESPath query to use in filtering the response data.')
+    .action(withCompatibilityCheck((options) => {
+        try {
+            new ListSkillsCommand(program).execute(options);
+        } catch (err) {
+            console.error(chalk.red(err.message));
+        }
+    }));
+
+// Save Skill
+program
+    .command('save <skillDefinition>')
+    .description('Save a skill definition')
+    .option('--no-compat', 'Ignore API compatibility checks')
+    .option('--color [on/off]', 'Turn on/off colors for JSON output.', 'on')
+    .option('--profile [profile]', 'The profile to use')
+    .option('--project [project]', 'The project to use')
+    .option('-y, --yaml', 'Use YAML for skill definition format')
+    .action(withCompatibilityCheck((skillDefinition, options) => {
+        try {
+            new SaveSkillCommand(program).execute(skillDefinition, options);
+        } catch (err) {
+            console.error(chalk.red(err.message));
+        }
+    }));
+
+// Undeploy Skill
+program
+    .command('undeploy <skillName>')
+    .description('Undeploy the skill resource from the cluster')
+    .option('--no-compat', 'Ignore API compatibility checks')
+    .option('--profile [profile]', 'The profile to use')
+    .option('--project [project]', 'The project to use')
+    .action(withCompatibilityCheck((skillName, options) => {
+        try {
+            new UndeploySkillCommand(program).execute(skillName, options);
         } catch (err) {
             console.error(chalk.red(err.message));
         }
