e6a3d896912687064ebefc57bca5f43429460b71
Akarsh Prasad <aprasad@cognitivescale.com>
2021-06-22 19:30:31 +0530

    display version, created and modelId on model save

--- src/commands/models.js
+++ src/commands/models.js
@@ -17,6 +17,7 @@ const fs = require('fs');
 
 const _ = {
     get: require('lodash/get'),
+    pick: require('lodash/pick'),
 };
 const debug = require('debug')('cortex:cli');
 const moment = require('moment');
@@ -45,7 +46,8 @@ module.exports.SaveModelCommand = class SaveModelCommand {
         const models = new Models(profile.url);
         models.saveModel(options.project || profile.project, profile.token, model).then((response) => {
             if (response.success) {
-                printSuccess('Model saved', options);
+                printSuccess('Model saved');
+                printSuccess(JSON.stringify(_.pick(response.message, ['version', 'created', 'modelId']), null, 2));
             } else if (response.details) {
                 console.log(`Failed to save model: ${response.status} ${response.message}`);
                 console.log('The following issues were found:');
