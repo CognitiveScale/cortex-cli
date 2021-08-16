96e76c82a4940d3289a0f3b50f0cf2df936b95c5
Johan Gielstra <jgielstra@cognitivescale.com>
2021-08-16 15:33:18 -0500

    FAB-2661 Fix --jobTimeout option

--- bin/cortex-actions.js
+++ bin/cortex-actions.js
@@ -118,7 +118,7 @@ program
     .option('--cmd [cmd]', 'Command to be executed') // '["--daemon"]'
     .option('--image, --docker [image]', 'Docker image to use as the runner')
     .option('--environmentVariables [environmentVariables]', 'Docker container environment variables, only used for daemon action types')
-    .option('--jobTimeout', 'Job Timeout in seconds, this will marked the job as FAILED')
+    .option('--jobTimeout [jobTimeout]', 'Job Timeout in seconds, this will marked the job as FAILED (default: no timeout)')
     .option('--no-compat', 'Ignore API compatibility checks')
     .option('--podspec [podspec]', 'A file containing either a JSON or YAML formatted pod spec to merge with the action definition, used for specifying resources (like memory, ephemeral storage, CPUs, and GPUs) and tolerations (like allowing pods to be scheduled on tainted nodes).')
     .option('--port [port]', 'Docker port') // '9091'
