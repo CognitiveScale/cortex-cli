8e28e1fbe1bd599704b13fe6bb55272834e6ab9c
Darren Schueller <dschueller@cognitivescale.com>
2021-11-15 12:25:52 -0500

    * Fixed up support for multiple config versions. * Added new config version 4, which includes template repo config and IDE registry support.

--- src/commands/configure.js
+++ src/commands/configure.js
@@ -74,13 +74,9 @@ module.exports.ConfigureCommand = class {
  //       });
     }
 
-    saveConfig(config, profileName, {
-         url, username, issuer, audience, jwk,
-    }, project) {
+    saveConfig(config, profileName, cfg, project) {
         if (!config) config = defaultConfig();
-        config.setProfile(profileName, {
-            url, username, issuer, audience, jwk, project,
-        });
+        config.setProfile(profileName, cfg, project);
         config.currentProfile = profileName;
         config.save();
     }
