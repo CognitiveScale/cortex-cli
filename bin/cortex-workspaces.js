6d56e960f3d6f853d6fa7e7a7e35bee12130508d
dschueller-cs <dschueller-cs@expresstoyou.com>
2022-02-22 15:05:18 -0500

    Remove test command for now

--- bin/cortex-workspaces.js
+++ bin/cortex-workspaces.js
@@ -141,19 +141,19 @@ program
         }
     });
 
-    program
-    .command('test [folder]')
-    .option('--color [on/off]', 'Turn on/off colors', 'on')
-    .option('--skill [skill name]', 'Test only the specified skill')
-    .option('--input [input name]', 'Test only the specified input')
-    .description('Tests the specified skill locally')
-    .action((folder, options) => { // deliberately not using withCompatibilityCheck()
-        try {
-            new WorkspaceTestCommand(program).execute(folder, options);
-        } catch (err) {
-            console.error(chalk.red(err.message));
-        }
-    });
+    // program
+    // .command('test [folder]')
+    // .option('--color [on/off]', 'Turn on/off colors', 'on')
+    // .option('--skill [skill name]', 'Test only the specified skill')
+    // .option('--input [input name]', 'Test only the specified input')
+    // .description('Tests the specified skill locally')
+    // .action((folder, options) => { // deliberately not using withCompatibilityCheck()
+    //     try {
+    //         new WorkspaceTestCommand(program).execute(folder, options);
+    //     } catch (err) {
+    //         console.error(chalk.red(err.message));
+    //     }
+    // });
 
 if (require.main === module) {
     program.showHelpAfterError().parseAsync(process.argv);
