b14d759cfd9594d4eb1a41cc043d283c5ab77e75
David Aldridge <daldridge@cognitivescale.com>
2022-04-07 15:53:55 -0500

    [FAB-1907] Add Project delete command

--- bin/cortex-projects.js
+++ bin/cortex-projects.js
@@ -25,6 +25,7 @@ const {
     CreateProjectCommand,
     ListProjectsCommand,
     DescribeProjectCommand,
+    DeleteProjectCommand,
 } = require('../src/commands/projects');
 
 program.name('cortex projects');
@@ -84,6 +85,22 @@ program
     .action(withCompatibilityCheck((projectName, options) => {
         try {
             new DescribeProjectCommand(program).execute(projectName, options);
+        } catch (err) {
+            console.error(chalk.red(err.message));
+        }
+    }));
+
+    // Delete Project
+program
+    .command('delete <projectName>')
+    .alias('d')
+    .description('Delete project')
+    .option('--no-compat', 'Ignore API compatibility checks')
+    .option('--color [on/off]', 'Turn on/off colors for JSON output.', 'on')
+    .option('--profile [profile]', 'The profile to use')
+    .action(withCompatibilityCheck((projectName, options) => {
+        try {
+            new DeleteProjectCommand(program).execute(projectName, options);
         } catch (err) {
             console.error(chalk.red(err.message));
         }
