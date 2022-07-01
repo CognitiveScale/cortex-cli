e205a018e10567f3e32171b081167cc9e2ab3adb
Johan Gielstra <jgielstra@cognitivescale.com>
2022-07-01 09:55:55 -0500

    Fix typo in code

--- src/commands/experiments.js
+++ src/commands/experiments.js
@@ -19,7 +19,6 @@ const _ = require('lodash');
 const debug = require('debug')('cortex:cli');
 const dayjs = require('dayjs');
 const relativeTime = require('dayjs/plugin/relativeTime');
-const relativeTime = require('dayjs/plugin/relativeTime');
 const duration = require('dayjs/plugin/duration');
 const { loadProfile } = require('../config');
 const Experiments = require('../client/experiments');
