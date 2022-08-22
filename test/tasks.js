aed8a5fb51b47f324d6e803e8dd9eae40902eb8e
Akarsh <aprasad@c12.com>
2022-08-22 14:47:48 +0530

    added test to verify --query works like a --json alias

--- test/tasks.js
+++ test/tasks.js
@@ -137,6 +137,48 @@ describe('Tasks', () => {
         nock.isDone();
     });
 
+    it('list tasks command QUERY should work as JSON alias without JSMEsearch', async () => {
+        const program = require('../bin/cortex-tasks');
+        const response = { success: true, tasks: ['task0', 'task1'] };
+        nock(serverUrl).get(/\/fabric\/v4\/projects\/.*\/tasks.*/).reply(200, response);
+        await program.parseAsync(['node', 'tasks', 'list', '--project', PROJECT, '--query']);
+        const output = getPrintedLines();
+        const errs = getErrorLines();
+        chai.expect(output.join('')).to.contain('name');
+        chai.expect(output.join('')).to.contain('task0');
+        chai.expect(output.join('')).to.contain('task1');
+        // eslint-disable-next-line no-unused-expressions
+        chai.expect(errs).to.be.empty;
+        nock.isDone();
+    });
+
+    it('list tasks command QUERY should work as JSON alias with JSMEsearch', async () => {
+        const program = require('../bin/cortex-tasks');
+        const now = Date.now();
+        const response = {
+            success: true,
+            tasks: [
+                {
+                    name: 'task0', startTime: now - (30 * 60000), endTime: now - (10 * 60000), activationId: 'xxxxxx1', skillName: 'skill1', actionName: 'action1', status: 'COMPLETE',
+                },
+                {
+                    name: 'task1', startTime: now - (40 * 60000), endTime: now - (10 * 60000), activationId: 'xxxxxx2', skillName: 'skill2', actionName: 'action1', status: 'FAIL',
+                },
+            ],
+        };
+        nock(serverUrl).get(/\/fabric\/v4\/projects\/.*\/tasks.*/).reply(200, response);
+        await program.parseAsync(['node', 'tasks', 'list', '--project', PROJECT, '--query', '[].{skillName: skillName, name: name}']);
+        const output = getPrintedLines();
+        const errs = getErrorLines();
+        chai.expect(output.join('')).to.not.contain('activationId');
+        chai.expect(output.join('')).to.contain('skillName');
+        chai.expect(output.join('')).to.contain('task0');
+        chai.expect(output.join('')).to.contain('task1');
+        // eslint-disable-next-line no-unused-expressions
+        chai.expect(errs).to.be.empty;
+        nock.isDone();
+    });
+
     it('list tasks command', async () => {
         const program = require('../bin/cortex-tasks');
         const now = Date.now();
