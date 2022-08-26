8e0e315924159c8aa880d90e963f7b11aa1bde6c
Akarsh <aprasad@c12.com>
2022-08-26 14:16:33 +0530

    added more tests

--- test/testUtilFunctions.js
+++ test/testUtilFunctions.js
@@ -1,5 +1,10 @@
 const { expect } = require('chai');
-const { getQueryOptions } = require('../src/commands/utils');
+const _ = require('lodash');
+const mockedEnv = require('mocked-env');
+const sinon = require('sinon');
+const { getQueryOptions, getFilteredOutput, filterObject } = require('../src/commands/utils');
+
+const { stripAnsi } = require('./utils');
 
 describe('Test getQueryOptions function', () => {
     it('should have empty query string if options.json is true', () => {
@@ -19,7 +24,9 @@ describe('Test getQueryOptions function', () => {
         expect(queryOptions.query).to.eql(queryPath);
     });
 
-    it('should override --json [arg] the if --query arg is passed', () => {
+    // TODO remove --query on deprecation
+    // handle cases 'cortex entity command --query <path> --json'
+    it('--query [arg] should override --json [arg] the if both are passed', () => {
         const queryPath = '[].{name: name, title: title}';
         const jsonPath = '[].{name: name}';
         const options = {
@@ -28,5 +35,48 @@ describe('Test getQueryOptions function', () => {
         };
         const queryOptions = getQueryOptions(options);
         expect(queryOptions.query).to.eql(queryPath);
+    });
+});
+
+describe('Test getFilteredOutput function', () => {
+    let restore;
+    let printSpy;
+    before(() => {
+        restore = mockedEnv({});
+    });
+
+    beforeEach(() => {
+        printSpy = sinon.spy(process.stderr, 'write');
+    });
+
+    afterEach(() => {
+        printSpy.restore();
+    });
+
+    after(() => {
+        restore();
+    });
+
+    function getPrintedLines() {
+        return _.flatten(printSpy.args).map((s) => stripAnsi(s));
+    }
+
+    it('should print Deprecation warning when query option is present', () => {
+        const queryPath = '[].{name: name, title: title}';
+        const options = {
+            query: queryPath,
+        };
+        getFilteredOutput([], options);
+        expect(getPrintedLines()).to.eql(['[DEPRECATION WARNING] --query\n']);
+    });
+
+    it('should print error message when query has invalid input', () => {
+        // handle cases 'cortex entity command --query <path> --json'
+        const queryPath = '--json';
+        const options = {
+            query: queryPath,
+        };
+        getFilteredOutput([], options);
+        expect(getPrintedLines()[1]).to.eql(`error: option '--query <query>' has an invalid argument: ${queryPath} \n`);
     });
 });
