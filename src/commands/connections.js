72e0db4bb898eeaf65bef5c27183ceebeed50ee8
Yashwanth <yreddy@cognitivescale.com>
2023-12-29 15:21:24 +0530

    fix lint issues

--- src/commands/connections.js
+++ src/commands/connections.js
@@ -18,7 +18,6 @@ import {
     handleListFailure,
     handleDeleteFailure,
     getFilteredOutput,
-    printErrorDetails,
 } from './utils.js';
 
 const debug = debugSetup('cortex:cli');
@@ -171,6 +170,7 @@ export class ListConnectionsTypes {
         debug('%s.listConnectionsTypes()', profile.name);
         const conns = new Connections(profile.url);
         conns.listConnectionsTypes(profile.token, options.limit, options.skip, options.sort)
+        // eslint-disable-next-line consistent-return
         .then((response) => {
         if (response.success) {
             const result = response.connectionTypes;
@@ -206,11 +206,13 @@ export class ListConnectionsTypes {
                     },
                 ];
                 handleTable(tableSpec, result, null, 'No connection types found');
-            }} else {
+            } 
+        } else {
                 return handleListFailure(response, options, 'Connections');
             } 
         })
             .catch((err) => {
             printError(`Failed to list connection types: ${err.status} ${err.message}`, options);
         });
-    }}
+    }
+}
