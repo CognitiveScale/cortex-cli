ef776d261f6187f3720e30bf899e3e09aca847aa
Johan Gielstra <jgielstra@cognitivescale.com>
2024-01-04 16:43:17 -0600

    Fix scan

--- build.sh
+++ build.sh
@@ -41,7 +41,7 @@ function docker_build(){
     npm ci --unsafe-perm --userconfig=/root/.npmrc --ignore-scripts
     npm rebuild
     npm test
-    npm scan
+    npm run scan
     echo ${VERSION} > version.txt
     ./generate_docs.sh
     BRANCH=$(git symbolic-ref --short -q HEAD)
